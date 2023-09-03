1c5319346dddde0ab4eb3d04328d5e1b4b275cd7
Hancie123 <hanciewanemphago@gmail.com>
2023-09-03 14:49:11 +0545

    commit change

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,11 +8,12 @@
     <meta name="viewport"
         content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-    <title>Dashboard - Analytics | Sneat - Bootstrap 5 HTML Admin Template - Pro</title>
+    <title>Dashboard - Om Kalpataru Carriers Pvt.Ltd</title>
 
     <meta name="description" content="" />
 
     @include('layouts/adminheader')
+
 </head>
 
 <body>
@@ -31,7 +32,7 @@
 
                     <div class="container-xxl flex-grow-1 container-p-y">
                         <div class="row">
-                            <div class="col-lg-8 mb-4 order-0">
+                            <div class="col-12 mb-4 order-0">
                                 <div class="card">
                                     <div class="d-flex align-items-end row">
                                         <div class="col-sm-7">
@@ -39,14 +40,13 @@
                                                 <h5 class="card-title text-primary"><span id="greeting-icon"
                                                         class="greeting-icon"></span> <span id="greeting"></span>
                                                     {{Session()->get('name')}}</h5>
-                                                <p class="mb-4">
-                                                    You have done <span class="fw-bold">72%</span> more sales today.
-                                                    Check your new badge in
-                                                    your profile.
+                                                <p class="mb-4 text-dark">
+                                                    You have {{count($contact)}} inbox message(s). 
+                                                    If you want to view all the inbox messages then click o below button.
                                                 </p>
 
-                                                <a href="javascript:;" class="btn btn-sm btn-outline-primary">View
-                                                    Badges</a>
+                                                <a href="{{url('/home/contact/view')}}" class="btn btn-sm btn-primary">View
+                                                    Inbox</a>
                                             </div>
                                         </div>
                                         <div class="col-sm-5 text-center text-sm-left">
@@ -63,72 +63,7 @@
 
 
 
-                            <div class="col-lg-4 col-md-4 order-1">
-                                <div class="row">
-                                    <div class="col-lg-6 col-md-12 col-6 mb-4">
-                                        <div class="card">
-                                            <div class="card-body">
-                                                <div
-                                                    class="card-title d-flex align-items-start justify-content-between">
-                                                    <div class="avatar flex-shrink-0">
-                                                        <img src="../assets/img/icons/unicons/chart-success.png"
-                                                            alt="chart success" class="rounded" />
-                                                    </div>
-                                                    <div class="dropdown">
-                                                        <button class="btn p-0" type="button" id="cardOpt3"
-                                                            data-bs-toggle="dropdown" aria-haspopup="true"
-                                                            aria-expanded="false">
-                                                            <i class="bx bx-dots-vertical-rounded"></i>
-                                                        </button>
-                                                        <div class="dropdown-menu dropdown-menu-end"
-                                                            aria-labelledby="cardOpt3">
-                                                            <a class="dropdown-item" href="javascript:void(0);">View
-                                                                More</a>
-                                                            <a class="dropdown-item"
-                                                                href="javascript:void(0);">Delete</a>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <span class="fw-semibold d-block mb-1">Profit</span>
-                                                <h3 class="card-title mb-2">$12,628</h3>
-                                                <small class="text-success fw-semibold"><i
-                                                        class="bx bx-up-arrow-alt"></i> +72.80%</small>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-6 col-md-12 col-6 mb-4">
-                                        <div class="card">
-                                            <div class="card-body">
-                                                <div
-                                                    class="card-title d-flex align-items-start justify-content-between">
-                                                    <div class="avatar flex-shrink-0">
-                                                        <img src="../assets/img/icons/unicons/wallet-info.png"
-                                                            alt="Credit Card" class="rounded" />
-                                                    </div>
-                                                    <div class="dropdown">
-                                                        <button class="btn p-0" type="button" id="cardOpt6"
-                                                            data-bs-toggle="dropdown" aria-haspopup="true"
-                                                            aria-expanded="false">
-                                                            <i class="bx bx-dots-vertical-rounded"></i>
-                                                        </button>
-                                                        <div class="dropdown-menu dropdown-menu-end"
-                                                            aria-labelledby="cardOpt6">
-                                                            <a class="dropdown-item" href="javascript:void(0);">View
-                                                                More</a>
-                                                            <a class="dropdown-item"
-                                                                href="javascript:void(0);">Delete</a>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <span>Sales</span>
-                                                <h3 class="card-title text-nowrap mb-1">$4,679</h3>
-                                                <small class="text-success fw-semibold"><i
-                                                        class="bx bx-up-arrow-alt"></i> +28.42%</small>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                            
 
 
                         </div>
@@ -185,6 +120,7 @@
     </script>
 
     @include('layouts/adminfooter')
+    
 </body>
 
 </html>
--- resources/views/staffs.blade.php
+++ resources/views/staffs.blade.php
@@ -37,6 +37,20 @@
     }
     </style>
 
+    <!-- Page Header Start -->
+    <div class="container-fluid page-header py-3 mb-3 wow fadeIn" data-wow-delay="0.1s">
+        <div class="container text-center py-3">
+            <h1 class=" text-white mb-2 animated slideInDown">Company Members</h1>
+            <nav aria-label="breadcrumb animated slideInDown">
+                <ol class="breadcrumb justify-content-center mb-0">
+                    <li class="breadcrumb-item"><a href="{{url('/')}}" wire:navigate>Home</a></li>
+                    <li class="breadcrumb-item text-primary" aria-current="page">members</li>
+                </ol>
+            </nav>
+        </div>
+    </div>
+    <!-- Page Header End -->
+
 
 
 
